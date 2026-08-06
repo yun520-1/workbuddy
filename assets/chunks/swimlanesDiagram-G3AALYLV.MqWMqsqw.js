e147521e72a6fcb910c2c8de381ccb146831e1ad
yun520-1 <yun520-1@users.noreply.github.com>
2026-08-06 18:17:25 +0800

    style: 暗色背景加墨绿调+光晕减淡，更耐看

---
+++ assets/chunks/swimlanesDiagram-G3AALYLV.MqWMqsqw.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.A-VsgdTP.js";import{_ as a}from"./mermaid.core.DPkUyvZn.js";import"./chunk-5VM5RSS4.D2CNGfa1.js";import"./chunk-XXDRQBXY.Bm2_JizQ.js";import"./chunk-VR4S4FIN.Bz8JQBHc.js";import"./chunk-32BRIVSS.DzoJKwf3.js";import"./channel.CqVmSLte.js";import"./framework.Bi3alPNc.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
